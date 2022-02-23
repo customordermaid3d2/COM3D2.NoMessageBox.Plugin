983762fb6156413e3e3e3325ad67b3fd6e1724b6
lilly <lilly1987@naver.com>
2022-02-23 10:59:20 +0900

    2.5 대응

---
+++ COM3D25.NoMessageBox.Plugin/NoMessageBox.cs
@@ -0,0 +1,47 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace COM3D2.NoMessageBox.Plugin
+{
+    class MyAttribute
+    {
+        public const string PLAGIN_NAME = "NoMessageBox";
+        public const string PLAGIN_VERSION = "22.2.23";
+        public const string PLAGIN_FULL_NAME = "COM3D2.NoMessageBox.Plugin";
+    }
+
+    [BepInPlugin(MyAttribute.PLAGIN_FULL_NAME, MyAttribute.PLAGIN_FULL_NAME, MyAttribute.PLAGIN_VERSION)]// 버전 규칙 잇음. 반드시 2~4개의 숫자구성으로 해야함. 미준수시 못읽어들임
+
+    public class NoMessageBox : BaseUnityPlugin
+    {
+
+        Harmony harmony;
+        public static ManualLogSource Log;
+
+        public void Awake()
+        {
+            Log = Logger;
+            Logger.LogMessage("Awake");
+        }
+
+        public void OnEnable()
+        {
+            harmony = Harmony.CreateAndPatchAll(typeof(NDebugPatch));
+        }
+
+        public void OnDisable()
+        {
+            harmony?.UnpatchSelf();
+        }
+    }
+}
---
+++ COM3D25.NoMessageBox.Plugin/NoMessageBoxPatch.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace COM3D2.NoMessageBox.Plugin
+{
+    class NDebugPatch
+    {
+        [HarmonyPatch(typeof(NDebug), "MessageBox"), HarmonyPrefix]
+        private static bool MessageBox(string f_strTitle, string f_strMsg) // string __m_BGMName 못가져옴
+        {
+            NoMessageBox.Log.LogFatal($"{f_strTitle} , {f_strMsg}");
+            return false;
+        }
+    }
+}
